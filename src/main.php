8e2209f7161debad8504eed3a3851193f769aa87
Peaumm <maxence.fouquet@gmail.com>
2024-09-26 16:47:31 +0200

    fix(main)

--- src/main.php
+++ src/main.php
@@ -9,31 +9,32 @@ use Jugid\Staurie\Component\Menu\Menu;
 use Jugid\Staurie\Component\PrettyPrinter\PrettyPrinter;
 use Jugid\Staurie\Staurie;
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once '../vendor/autoload.php';
 
-$staurie = new Staurie('Example lands');
+$staurie = new Staurie('Lord Regalius 2');
 $staurie->register([
-    Console::class, 
-    PrettyPrinter::class, 
-    MainCharacter::class, 
-    Inventory::class, 
+  Console::class, 
+  PrettyPrinter::class, 
+  MainCharacter::class, 
+  Inventory::class,
+  Menu::class
 ]);
 
 $container = $staurie->getContainer();
 
 $menu = $container->registerComponent(Menu::class);
 $menu->configuration([
-    'text'=> 'Welcome to this awesome test adventure',
-    'labels'=> [
-        'new_game' => 'Enter the world',
-        'quit'=> 'Exit game',
-    ]
+  'text'=> 'Bienvenue sur Lord Regalius 2',
+  'labels'=> [
+    'new_game' => 'Entrer dans le monde',
+    'quit'=> 'Quitter le jeu',
+  ]
 ]);
 
 $map = $container->registerComponent(Map::class);
 $map->configuration([
-    'directory'=>__DIR__.'/maps',
-    'namespace'=>'Jugid\Staurie\Example\Maps', 
+    'directory'=>__DIR__.'/Maps',
+    'namespace'=>'Peaumm\Donjon\Maps',
     'navigation'=>true,
     'map_enable'=>true,
     'compass_enable'=>true
@@ -49,4 +50,5 @@ $introduction->configuration([
     'scrolling'=>false
 ]);
 
-$staurie->run();
+
+$staurie->run(); //LANCE LE JEU
