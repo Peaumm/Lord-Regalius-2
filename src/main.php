72213ce33a19c21088f92f5b9e8708d6fd39b351
Peaumm <maxence.fouquet@gmail.com>
2024-09-26 11:28:55 +0200

    feat(main.php): add intro, map and inventory

--- src/main.php
+++ src/main.php
@@ -3,14 +3,25 @@
 use Jugid\Staurie\Component\Console\Console;
 use Jugid\Staurie\Component\Menu\Menu;
 use Jugid\Staurie\Component\PrettyPrinter\PrettyPrinter;
+use Jugid\Staurie\Component\Inventory\Inventory;
+use Jugid\Staurie\Component\Character\MainCharacter;
+use Jugid\Staurie\Component\Introduction\Introduction;
+use Jugid\Staurie\Component\Map\Map;
 use Jugid\Staurie\Staurie;
 
 require_once '../vendor/autoload.php'; //A REMPLACER
 
 $staurie = new Staurie('Lord Regalius 2');
-$staurie->register([Console::class, PrettyPrinter::class, Menu::class]);
+$staurie->register([
+  Console::class, 
+  PrettyPrinter::class, 
+  MainCharacter::class, 
+  Inventory::class,
+  Menu::class
+]);
 
 $container = $staurie->getContainer();
+
 $menu = $container->registerComponent(Menu::class);
 $menu->configuration([
   'text'=> 'Bienvenue sur Lord Regalius 2',
@@ -18,6 +29,25 @@ $menu->configuration([
     'new_game' => 'Entrer dans le monde',
     'quit'=> 'Quitter le jeu',
   ]
+]);
+
+$map = $container->registerComponent(Map::class);
+$map->configuration([
+    'directory'=>__DIR__.'/maps',
+    'namespace'=>'Jugid\Staurie\Example\Maps', 
+    'navigation'=>true,
+    'map_enable'=>true,
+    'compass_enable'=>true
+]);
+
+$introduction = $container->registerComponent(Introduction::class);
+$introduction->configuration([
+    'text'=>[
+        'This is an introduction to test the introduction component',
+        'You can use it multiline by using an array in configuration'
+    ],
+    'title'=>'Chapter 1 : The new game',
+    'scrolling'=>false
 ]);
 
 
